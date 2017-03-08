e7d1912e3e74d70afc20554e3a13bb3b7d4aee0f
Stephen Fluin <stephen.fluin@gmail.com>
2017-03-07 19:23:26 -0800

    feat: Material Design and minimal styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,6 @@ export interface Step {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular Upgrade Guide';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MdToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
