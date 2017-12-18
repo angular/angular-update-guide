fa7c7e125a8adb954886b928ce44c4208eec2c8f
Stephen Fluin <stephen.fluin@gmail.com>
2017-12-18 15:51:46 -0800

    Dependency bump

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -15,6 +14,7 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
