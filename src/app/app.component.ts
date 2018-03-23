d555db29824f31dcce040bbdfab6043eaae4fb59
Stephen Fluin <stephen.fluin@gmail.com>
2018-03-23 13:22:41 -0700

    Fix formatting issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from "@angular/core";
-import * as Showdown from "showdown";
-import { Step, RECOMMENDATIONS } from "./recommendations";
+import { Component } from '@angular/core';
+import * as Showdown from 'showdown';
+import { Step, RECOMMENDATIONS } from './recommendations';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html"
+  selector: 'app-root',
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = "Angular Update Guide";
+  title = 'Angular Update Guide';
 
   level: number = 1;
   ngUpgrade: boolean;
@@ -22,30 +22,31 @@ export class AppComponent {
   converter: Showdown.Converter;
 
   versions = [
-    { name: "2.0", number: 200 },
-    { name: "2.1", number: 201 },
-    { name: "2.2", number: 202 },
-    { name: "2.3", number: 203 },
-    { name: "2.4", number: 204 },
-    { name: "4.0", number: 400 },
-    { name: "4.1", number: 410 },
-    { name: "4.2", number: 420 },
-    { name: "4.3", number: 430 },
-    { name: "4.4", number: 440 },
-    { name: "5.0", number: 500 },
-    { name: "6.0", number: 600 },
-    { name: "7.0", number: 600 }
+    { name: '2.0', number: 200 },
+    { name: '2.1', number: 201 },
+    { name: '2.2', number: 202 },
+    { name: '2.3', number: 203 },
+    { name: '2.4', number: 204 },
+    { name: '4.0', number: 400 },
+    { name: '4.1', number: 410 },
+    { name: '4.2', number: 420 },
+    { name: '4.3', number: 430 },
+    { name: '4.4', number: 440 },
+    { name: '5.0', number: 500 },
+    { name: '5.1', number: 510 },
+    { name: '5.2', number: 520 },
+    { name: '6.0', number: 600 },
+    { name: '7.0', number: 700 },
+    { name: '8.0', number: 800 },
   ];
-  to = this.versions[10];
+  to = this.versions[12];
   from = this.versions[5];
   steps: Step[] = RECOMMENDATIONS;
 
   constructor() {
     this.converter = new Showdown.Converter();
 
-    this.steps.map(
-      item => (item.renderedStep = this.converter.makeHtml(item.action))
-    );
+    this.steps.map(item => (item.renderedStep = this.converter.makeHtml(item.action)));
   }
 
   showUpdatePath() {
@@ -53,13 +54,12 @@ export class AppComponent {
     this.duringRecommendations = [];
     this.afterRecommendations = [];
 
-    this.title = `Angular Update Guide - ${this.from.name} -> ${this.to
-      .name} for ${this.level < 2
-      ? "Basic"
-      : this.level < 3 ? "Medium" : "Advanced"} Apps`;
+    this.title = `Angular Update Guide - ${this.from.name} -> ${this.to.name} for ${
+      this.level < 2 ? 'Basic' : this.level < 3 ? 'Medium' : 'Advanced'
+    } Apps`;
 
     // Find applicable steps and organize them into before, during, and after upgrade
-    for (let step of this.steps) {
+    for (const step of this.steps) {
       if (
         step.level <= this.level &&
         step.necessaryAsOf > this.from.number &&
@@ -68,21 +68,15 @@ export class AppComponent {
         (!step.pwa || this.pwa)
       ) {
         // If you could do it before now, but didn't have to finish it before now
-        if (
-          step.possibleIn <= this.from.number &&
-          step.necessaryAsOf >= this.from.number
-        ) {
+        if (step.possibleIn <= this.from.number && step.necessaryAsOf >= this.from.number) {
           this.beforeRecommendations.push(step);
           // If you couldn't do it before now, and you must do it now
-        } else if (
-          step.possibleIn > this.from.number &&
-          step.necessaryAsOf <= this.to.number
-        ) {
+        } else if (step.possibleIn > this.from.number && step.necessaryAsOf <= this.to.number) {
           this.duringRecommendations.push(step);
         } else if (step.possibleIn <= this.to.number) {
           this.afterRecommendations.push(step);
         } else {
-          console.log("ignoring irrelevant step", step);
+          console.log('ignoring irrelevant step', step);
         }
       }
     }
@@ -92,36 +86,49 @@ export class AppComponent {
     const isWindows = /win/i.test(navigator.platform);
     const additionalDeps = this.getAdditionalDependencies(this.to.number);
     const angularVersion = this.getAngularVersion(this.to.number);
-    const angularPackages = ['animations','common','compiler','compiler-cli','core','forms','http','platform-browser','platform-browser-dynamic','platform-server','router'];
+    const angularPackages = [
+      'animations',
+      'common',
+      'compiler',
+      'compiler-cli',
+      'core',
+      'forms',
+      'http',
+      'platform-browser',
+      'platform-browser-dynamic',
+      'platform-server',
+      'router',
+    ];
 
-    const actionMessage = `Update all of your dependencies to the latest Angular and the right version of TypeScript.`
+    const actionMessage = `Update all of your dependencies to the latest Angular and the right version of TypeScript.`;
 
     if (isWindows) {
-      const packages = angularPackages.map(packageName => `@angular/${packageName}@${angularVersion}`).join(' ') + ' ' + additionalDeps;
+      const packages =
+        angularPackages.map(packageName => `@angular/${packageName}@${angularVersion}`).join(' ') +
+        ' ' +
+        additionalDeps;
 
       upgradeStep = {
-        step: "General Update",
-        action:
-          `${actionMessage}
+        step: 'General Update',
+        action: `${actionMessage}
           If you are using Windows, you can use:
 
-\`${this.packageManager} ${packages}\``
+\`${this.packageManager} ${packages}\``,
       };
     } else {
       const packages = `@angular/{${angularPackages.join(',')}}@${angularVersion} ${additionalDeps}`;
       upgradeStep = {
-        step: "General update",
-        action:
-        `${actionMessage}
+        step: 'General update',
+        action: `${actionMessage}
           If you are using Linux/Mac, you can use:
 
-\`${this.packageManager} ${packages}\``
+\`${this.packageManager} ${packages}\``,
       };
     }
 
     // Npm installs typescript wrong in v5, let's manually specify
     // https://github.com/npm/npm/issues/16813
-    if(this.packageManager === 'npm install' && this.to.number === 500) {
+    if (this.packageManager === 'npm install' && this.to.number === 500) {
       upgradeStep.action += `
 
 \`npm install typescript@2.4.2 --save-exact\``;
@@ -144,7 +151,7 @@ export class AppComponent {
       return `'^2.0.0'`;
     } else {
       const major = Math.floor(version / 100);
-      const minor = Math.floor( ( version - (major * 100) )  / 10);
+      const minor = Math.floor((version - major * 100) / 10);
       return `'^${major}.${minor}.0'`;
     }
   }
