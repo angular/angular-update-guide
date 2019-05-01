f36ff4bdc310232e890fc0885e098d46e10689de
Stephen Fluin <stephen.fluin@gmail.com>
2019-05-01 04:18:14 -0600

    chore: bump to version 8 RC of Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
-import {
-  MatButtonModule, MatCardModule,
-  MatCheckboxModule, MatGridListModule,
-  MatInputModule,
-  MatListModule,
-  MatSelectModule,
-  MatToolbarModule,
-  MatProgressBarModule,
-  MatButtonToggleModule
-} from '@angular/material';
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatSelectModule } from "@angular/material/select";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
