fc3ee3cedba4bcf43442b011eeb556c1177ef8ac
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-11-12 22:28:30 +0900

    update Japanese locale for v11.0

--- src/app/locales/ja_JP.ts
+++ src/app/locales/ja_JP.ts
@@ -141,6 +141,115 @@ const steps: LocalizedSteps = {
     action:
       'サポート終了、自動マイグレーション、変更の詳細については、[angular.ioのガイド](https://angular.jp/guide/updating-to-version-10)にアクセスしてください。',
   },
+  // v10.2
+  'universal-baseurl': {
+    action:
+      'Angular Universalユーザーの場合、 `useAbsoluteUrl`を使用して`platform-server`をセットアップする場合は、 `baseUrl`も指定する必要があります。',
+  },
+  // v11.0
+  'v11 ng update': {
+    action: '`ng update @angular/core @angular/cli`を実行すると、Angularのバージョンが11になります。',
+  },
+  'v11 versions': {
+    action:
+      'Angularは [TypeScript 4.0](https://devblogs.microsoft.com/typescript/announcing-typescript-4-0/)が必要になりました。ng updateで自動的に移行します。',
+  },
+  'v11 browser support': {
+    action:
+      'IE9、IE10、IE mobileのサポートが終了しました。これは[v10 update](https://blog.angular.io/version-10-of-angular-now-available-78960babd41#c357)で発表されました。',
+  },
+  'webpack5 optin': {
+    action:
+      'Yarnを使って`"resolutions": {"webpack". "^5.0.0"}` を `package.json` に追加することで、webpack 5を使用するようにオプトインすることができます。',
+  },
+  'ng new strict prompt': {
+    action:
+      '新しいプロジェクトを生成するときに、厳密モードを有効にするかどうかを尋ねられます。これにより、TypeScript と Angular コンパイラがより厳密な型チェックを行うように設定され、デフォルトではより小さなバンドル予算が適用されます。プロンプトをスキップするには、`--strict=true` または `--strict=false` を使うことができます。',
+  },
+  'v11 router relativeLinkResolution': {
+    action:
+      'Routerを使用している場合、`relativeLinkResolution` のデフォルト値が `legacy` から `corrected` に変更されています。アプリケーションがExtraOptionsで値を指定せずにデフォルトを使用していて、空のパスルートの子からナビゲートする際に相対リンクを使用している場合は、`RouterModule`の設定を更新して `legacy` を `relativeLinkResolution` に明示的に指定する必要があります。詳細は [ドキュメント](https://v11.angular.io/api/router/ExtraOptions#relativeLinkResolution) を参照してください。',
+  },
+  'router initialNavigation': {
+    action:
+      'Angular Router では、v4 で非推奨とされていた `initialNavigation` のオプションが削除されました。以前に `enabled` または `true` を使用していた場合は、`enabledNonBlocking` または `enabledBlocking` を選択してください。以前に `false` または `legacy_disabled` を使用していた場合は、`disabled` を使用してください。',
+  },
+  'routerlink preserveQueryParams': {
+    action:
+      'Angular Routerの`routerLink`では、`preserveQueryParams`が削除されているので、代わりに`queryParamsHandling="preserve"`を使用してください。',
+  },
+  'routerlink queryParams typing': {
+    action:
+      'queryParams`, `fragment` や `queryParamsHandling` の `routerLink` の値にアクセスしている場合、`undefined` や `null` も受け付けるように型付けを緩和する必要があるかもしれません。',
+  },
+  'viewencapsulation native removed': {
+    action:
+      'コンポーネントビューのカプセル化オプション `ViewEncapsulation.Native` は削除されました。代わりに `ViewEncapsulation.ShadowDom` を使ってください。`ng update` が自動的に移行します。',
+  },
+  'ICU expressions typechecked': {
+    action:
+      'i18n を使用している場合、International Components for Unicode (ICU) 式内の式が再び型チェックされるようになりました。これにより、ICU内に現れる式にエラーが見つかった場合、コンパイルに失敗する可能性があります。',
+  },
+  'forms validators asyncValidators typing': {
+    action:
+      '以前は `@angular/forms` パッケージのディレクティブは、コンストラクタで期待される `validators` と `asyncValidators` の引数の型として `any[]` を持っていました。現在ではこれらの引数は適切に型付けされているので、もしあなたのコードがフォームのディレクティブのコンストラクタの型に依存しているのであれば、型の安全性を向上させるためにいくつかの更新が必要になるかもしれません。',
+  },
+  'forms AbstractFormControl': {
+    action:
+      'Angular Forms を使っている場合、`AbstractFormControl.parent` の型に null が含まれるようになりました。 `ng update`で自動的に移行されますが、もしコードがundefinedに対して厳密等価性をテストしていた場合は、これを `=== null` に変更する必要があります。',
+  },
+  'platform-webworker': {
+    action:
+      'あまり使われていなかった `@angular/platform-webworker` と `@angular/platform-webworker-dynamic` は v8 で非推奨となり削除されました。Angularの一部をWebWorkerで実行することは、一般的なユースケースでは決してうまくいかない実験でした。Angularは依然として[Web Worker](https://angular.io/guide/web-worker)をしっかりサポートしています。',
+  },
+  'v11 slice pipe typing': {
+    action: '`slice` パイプはunefinedの入力値に対して null を返すようになりました。',
+  },
+  'v11 keyvalue typing': {
+    action:
+      '`keyvalue` パイプが修正され、数値キーを持つオブジェクトの入力に対し、結果の型にはキーの文字列表現が含まれるようになりました。これはすでに適用されていたもので、コードは単純にこれを反映するように更新されました。パイプ出力の利用者が不正な型に依存していた場合は更新してください。これは入力値が `Map` である場合には影響しませんので、`number` を保持する必要がある場合には有効な方法です。',
+  },
+  'v11 number pipe typing': {
+    action:
+      '数値パイプ (`decimal`, `percent`, `currency` など) では、どの型が受け入れられるかが明示的に記述するようになりました。',
+  },
+  'v11 date pipe typing': { action: '`date` パイプは、どの型が受け入れられるかを明示的に記述するようになりました。' },
+  'v11 datetime rounding': {
+    action:
+      'ミリ秒単位の端数を含むフォーマットで日付時刻フォーマットの文字列を `DatePipe` に渡した場合、ミリ秒は常に直近のミリ秒ではなく切り捨てられるようになりました。ほとんどのアプリケーションでは、この変更の影響を受けることはありません。これが望ましい動作ではない場合は、文字列を `DatePipe` に渡す前にミリ秒の部分を丸めるように文字列を前処理することを検討してください。',
+  },
+  'v11 async pipe typing': {
+    action:
+      '`async`パイプは、undefined型の入力に対してundefinedを返さない型定義になりました。コードは実際にはundefinedの入力に対してnullを返していたことに注意してください。',
+  },
+  'v11 case pipe update': {
+    action:
+      '`uppercase`パイプと`lowercase`パイプは、falsyな値を通さないようになりました。これらのパイプは `null` と `undefined` の両方を `null` にマップし、無効な入力 (`0`, `false`, `NaN`) に対して例外を発生させるようになりました。これは他の Angular パイプと一致しています。',
+  },
+  'v11 router NavigationExtras typing': {
+    action:
+      'Routerの `NavigationExtras` を使う場合、新しい型付けでは `NavigationExtras` 型の変数を渡すことができますが、既知のプロパティしか指定できないため、オブジェクトリテラルを渡すことはできません。また、`Pick` のプロパティと共通のプロパティを持たない型も受け付けません。この変更の影響を受ける場合は、それぞれの関数呼び出しで実際に使用されるNavigationExtrasのプロパティのみを指定するか、次のようにオブジェクトや変数の型アサーションを使用してください: `as NavigationExtras`',
+  },
+  'v11 TestBed.overrideProvider': {
+    action:
+      'テストでTestBedの初期化後に `TestBed.overrideProvider` を呼び出すと、プロバイダのオーバーライドは適用されなくなります。この動作は他のオーバーライドメソッド(`TestBed.overrideDirective` など)と一致していますが、そのことを示すエラーをスローします。このチェックは以前はTestBed.overrideProvider関数にはありませんでした。このエラーが出た場合は、TestBedの初期化が完了する前に `TestBed.overrideProvider` の呼び出しを移動する必要があります。',
+  },
+  'v11 router RouteReuseStrategy': {
+    action:
+      'Routerの RouteReuseStrategy を使用している場合、引数の順序が変更されています。以前、子ルートを評価する際に `RouteReuseStrategy#shouldReuseRoute` を呼び出した場合、`future` と `current` の引数を入れ替えて呼び出されていました。`RouteReuseStrategy`が特に未来または現在のスナップショットの状態のみに依存している場合は、`shouldReuseRoute` の実装で使用する `future` と `current` の `ActivateRouteSnapshots` を更新する必要があるかもしれません。',
+  },
+  'v11 locale data readonly': {
+    action:
+      'ロケールデータの配列を使用している場合、このAPIはReadonlyな配列を返すようになりました。配列を改変していた場合 (例: `sort()`, `push()`, `splice()` など)、そのコードはコンパイルできなくなります。配列を改変する必要がある場合は、コピーして(例: `slice()`を呼び出して)、そのコピーを改変する必要があります。',
+  },
+  'v11 CollectionChangeRecord': {
+    action:
+      '変更検知では、`CollectionChangeRecord` が削除されたので、代わりに `IterableChangeRecord` を使用してください。',
+  },
+  'v11 forms async validators': {
+    action:
+      'Angular Formsで初期化時に非同期バリデータが定義されている `FormControl`, `FormGroup`, `FormArray` のクラスインスタンスを使用している場合、以前は非同期バリデータが完了してもステータス変更イベントは発生しませんでした。これが変更され、ステータスイベントが `statusChanges` Observableに出力されるようになりました。以前の振る舞いに依存している場合は、この追加のステータス変更イベントをフィルタリングしたり無視したりすることができます。',
+  },
 };
 // @TODO This console log is required or the locale won't be registered
 console.log(`ja-JP registered`);
